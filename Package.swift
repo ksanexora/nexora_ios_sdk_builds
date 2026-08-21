c1f1ea9922322e317cc86b821574d7c4dd690af3
ABINESH S <abineshsj28@gmail.com>
2026-08-21 17:28:29 +0530

    Fix SPM binary checksum

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .binaryTarget(
             name: "NexoraSDK",
             url: "https://github.com/ksanexora/nexora_ios_sdk_builds/releases/download/0.0.3/NexoraSDK.xcframework.zip",
-            checksum: "c9de7055a8b4df1e893b463273789e7bc7d3508783f30b09ead40149c518c228"
+            checksum: "d88899f27a41305ca916879a1815fc0476b42c7fe37b4d7bce3d432b7bd9a9fd"
         )
     ]
 )
