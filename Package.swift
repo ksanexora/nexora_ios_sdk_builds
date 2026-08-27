7a626ecca4e378980b2631f4bbcc55328a76297c
ABINESH S <abineshsj28@gmail.com>
2026-08-27 12:01:21 +0530

    v0.0.14

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "NexoraSDK",
-            url: "https://github.com/ksanexora/nexora_ios_sdk_builds/releases/download/0.0.13/NexoraSDK.xcframework.zip",
+            url: "https://github.com/ksanexora/nexora_ios_sdk_builds/releases/download/0.0.14/NexoraSDK.xcframework.zip",
             checksum: "71fe37f978d1f49736aa50a9f24326e13bb7d22d0d5146c549cc6121f8297442"
         )
     ]
